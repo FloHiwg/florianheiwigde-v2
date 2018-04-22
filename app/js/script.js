d86c9f5ab26b2638a7417142f01c835afc441abd
Heiwig Florian <Florian.Hiwg@gmail.com>
2018-04-22 23:28:50 +0200

    init commit

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    changeSkill();
+
+});
+
+var skillName = ['developer,', 'creative-mind,', 'digital all-rounder,']
+
+var skillColor = ['fh-orange', 'fh-blue', 'fh-red'];
+
+var roundCounter = 0;
+
+function changeSkill(){
+    console.log("method");
+    roundCounter++;
+    console.log(skillColor[(roundCounter - 1) % skillColor.length]);
+    $('.fh-tint').removeClass(skillColor[(roundCounter - 1) % skillColor.length]);
+    $('.fh-tint').addClass(skillColor[roundCounter % skillColor.length]);
+    $('.fh-skill').removeClass(skillColor[(roundCounter - 1) % skillColor.length]);
+    $('.fh-skill').addClass(skillColor[roundCounter % skillColor.length]);
+    $('.fh-skill').text(skillName[roundCounter % skillColor.length]);
+    setTimeout(function(){
+      changeSkill();
+    }, 2000);
+}
